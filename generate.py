9cbce9e1df789368645bbca3a462d8f77155b455
kielejocain <joecken@gmail.com>
2019-08-21 19:53:32 -0700

    Add interesting Strategy and update script

--- generate.py
+++ generate.py
@@ -1,14 +1,17 @@
 # A simple script to generate a set of students
 
 from generator.factory import Factory
-from generator.strategy import NoDemoStrategy
+from generator.strategy import DemoStrategy
 from generator.constants import DEMO
 import numpy as np
 
 # Choices
 n = 100000  # Number of students desired
+pov_cost = 0.05  # mean drop for student in poverty
+ell_cost = 0.05  # mean drop for english learners in reading and english
+dis_cost = 0.05  # max possible mean change for a student with a disability
 
-strat = NoDemoStrategy()
+strat = DemoStrategy(pov_cost, ell_cost, dis_cost)
 stud_fact = Factory(n, DEMO, strat)
 stud_fact.print_demos()
 
--- generator/strategy.py
+++ generator/strategy.py
@@ -127,3 +127,63 @@ class NoDemoStrategy(Strategy):
         sigma = 0.05
         return np.random.normal(mu, sigma)
 
+
+class DemoStrategy(Strategy):
+    """
+    """
+
+    def __init__(self, pov_cost, ell_cost, dis_cost):
+        self.pov_cost = pov_cost
+        self.ell_cost = ell_cost
+        self.dis_cost = dis_cost
+
+    def _compute_generic_factor(self, student):
+        """
+        The generic proficiency of a student gives a bonus or penalty to every subject.
+        :param student: A Student object
+        :return: A float multiplier.
+        """
+        mu = 1 - student.poverty * self.pov_cost
+        sigma = 0.05
+        return np.random.normal(mu, sigma)
+
+    def _compute_english_factor(self, student):
+        """
+        The English proficiency of a student gives a bonus or penalty to every subject.
+        :param student: A Student object
+        :return: A float multiplier.
+        """
+        mu = 1 - student.ell * self.ell_cost + student.disabled * np.random.uniform(-1, 1) * self.dis_cost
+        sigma = 0.05
+        return np.random.normal(mu, sigma)
+
+    def _compute_math_factor(self, student):
+        """
+        The Math proficiency of a student gives a bonus or penalty to every subject.
+        :param student: A Student object
+        :return: A float multiplier.
+        """
+        mu = 1 + student.disabled * np.random.uniform(-1, 1) * self.dis_cost
+        sigma = 0.05
+        return np.random.normal(mu, sigma)
+
+    def _compute_reading_factor(self, student):
+        """
+        The reading proficiency of a student gives a bonus or penalty to every subject.
+        :param student: A Student object
+        :return: A float multiplier.
+        """
+        mu = 1 - student.ell * self.ell_cost + student.disabled * np.random.uniform(-1, 1) * self.dis_cost
+        sigma = 0.05
+        return np.random.normal(mu, sigma)
+
+    def _compute_science_factor(self, student):
+        """
+        The Science proficiency of a student gives a bonus or penalty to every subject.
+        :param student: A Student object
+        :return: A float multiplier.
+        """
+        mu = 1 + student.disabled * np.random.uniform(-1, 1) * self.dis_cost
+        sigma = 0.05
+        return np.random.normal(mu, sigma)
+
